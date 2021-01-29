c4e52cc411f0f593b68d7a1daec9550fdb1f0ed2
siyoon <siyoon210@gmail.com>
2021-01-30 07:33:38 +0900

    0.8 Blockchain Creating a Block

--- typescript-blockchain-nomadcoder/src/index.ts
+++ typescript-blockchain-nomadcoder/src/index.ts
@@ -1,7 +1,21 @@
-const sayHi = (name: string, age: number, gender: string): void => {
-    console.log(`Hello! :) ${name}. ${gender} ${age}`)
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
 
-sayHi("siyoon", 32, "man")
+const genesisBlock: Block = new Block(0, "QWE123EGVC76@", "", "Hello", 123456);
 
-export {}
+const blockchain: [Block] = [genesisBlock];
+
+console.log(blockchain);
