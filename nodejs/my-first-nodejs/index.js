48fd1cb3dfc81b02d749537d65ef66f2235fa5f5
siyoon <siyoon210@gmail.com>
2021-02-08 02:31:13 +0900

    mariadb express 라우터 핸들러에서 사용하기

--- nodejs/my-first-nodejs/index.js
+++ nodejs/my-first-nodejs/index.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mariadb = require('mariadb');
+const dbConfig = require('./config/db-config.json')
+const pool = mariadb.createPool({
+    host: dbConfig.host,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database,
+    connectionLimit: dbConfig.connectionLimit
+});
+
+async function useMariaDB() {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const rows = await conn.query("SELECT * FROM TOPIC");
+        console.log(rows);
+        return rows;
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.release(); //release to pool
+    }
+}
 
 app.set("view engine", "pug");
 
-app.get("/", ((req, res) => {
-    res.render("index.pug");
+app.get("/", (async (req, res) => {
+    const result = await useMariaDB();
+    res.send(result);
 }))
 
 app.listen(port, () => {
