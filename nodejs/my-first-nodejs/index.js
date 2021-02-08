4ed3ea6c63001ff9f30bd94bf910b6c6eab798e7
siyoon <siyoon210@gmail.com>
2021-02-08 22:14:46 +0900

    topic 리스트 보여주기, topic 상세 보여주기

--- nodejs/my-first-nodejs/index.js
+++ nodejs/my-first-nodejs/index.js
@@ -11,11 +11,11 @@ const pool = mariadb.createPool({
     connectionLimit: dbConfig.connectionLimit
 });
 
-async function useMariaDB() {
+async function executeQuery(sql, values) {
     let conn;
     try {
         conn = await pool.getConnection();
-        const rows = await conn.query("SELECT * FROM TOPIC");
+        const rows = await conn.query(sql, values);
         console.log(rows);
         return rows;
     } catch (err) {
@@ -28,9 +28,17 @@ async function useMariaDB() {
 app.set("view engine", "pug");
 
 app.get("/", (async (req, res) => {
-    const result = await useMariaDB();
-    res.send(result);
+    const topics = await executeQuery(`SELECT id, title FROM topic`);
+    res.render('index.pug', {topics})
 }))
+
+app.get("/topics/:id", async (req, res) => {
+    const topics = await executeQuery(`SELECT id, title FROM topic`);
+    const id = req.params.id;
+    const sql = `SELECT t.id, t.title, t.description, t.created, t.author_id, a.name, a.profile FROM topic AS t JOIN author AS a ON t.author_id = a.id WHERE t.id = ?`
+    const topic = await executeQuery(sql, [id]);
+    res.render('topic.pug', {topics, topic: topic[0]})
+})
 
 app.listen(port, () => {
     console.log('my-first-nodejs app listening at http://localhost:3000')
